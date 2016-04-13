1a8386f228b462c04822f4972ee871caea1bdda0
mariansurdu <surdu_marian94@yahoo.com>
2016-04-14 00:01:45 +0300

    'Finish'

--- smp.asm
+++ smp.asm
@@ -207,7 +207,45 @@ w equ 20 ; dimensiune dreptunghi
 h equ 6
 new: mov ah, 0
  mov al, 13h ; trecere in mod grafic 320x200
- int 10h 
+ int 10h
+ 
+;robinet
+ ;lat sup robinet
+; afisare latura inferioare
+ mov cx, 230+w
+ mov dx, 130+h
+ mov al, 35
+u23: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 153
+ ja u23 
+ 
+ 
+ mov cx, 230+w
+ mov dx, 140+h
+ mov al, 40
+u231: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 153
+ ja u231
+ 
+ 
+ ; latura din stanga
+ mov cx, 150
+ mov dx, 140+h
+ mov al, 30
+u333: mov ah, 0ch
+ int 10h
+ dec dx
+ cmp dx, 135
+ ja u333       
+
+
+
+;teava alimentare 
+  
  
  ; afisare latura inferioare
  mov cx, 200+w
@@ -227,7 +265,7 @@ u2: mov ah, 0ch
 u3: mov ah, 0ch
  int 10h
  dec dx
- cmp dx, 100
+ cmp dx, 80
  ja u3                     
  
  
@@ -239,7 +277,7 @@ u3: mov ah, 0ch
  u4: mov ah, 0ch
  int 10h
  dec dx
- cmp dx, 100
+ cmp dx, 80
  ja u4
  
 ; afisare latura superioara
@@ -250,7 +288,337 @@ u1: mov ah, 0ch ; afisare pixel
  int 10h
  dec cx
  cmp cx, 100
- jae u1
+ jae u1  
+ 
+ ;aducere apa in incinta
+ 
+ mov cx, 230+w ; coloana
+ mov dx, 144 ; rand
+ mov al, 30 ; culoare
+u11111: mov ah, 0ch ; afisare pixel
+ int 10h
+ dec cx
+ cmp cx, 145
+ jae u11111
+ 
+ 
+  mov cx, 230+w ; coloana
+ mov dx, 143 ; rand
+ mov al, 30 ; culoare
+u1111112: mov ah, 0ch ; afisare pixel
+ int 10h
+ dec cx
+ cmp cx, 145
+ jae u1111112
+ 
+ 
+ ;incepe sa se umpe bazinul
+ 
+ mov cx, 200+w
+ mov dx, 149+h
+ mov al, 30
+u2149: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2149
+ 
+  mov cx, 200+w
+ mov dx, 148+h
+ mov al, 30
+u214: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u214
+ 
+  mov cx, 200+w
+ mov dx, 147+h
+ mov al, 30
+u2147: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2147
+ 
+  mov cx, 200+w
+ mov dx, 146+h
+ mov al, 30
+u2146: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2146
+ 
+ 
+  mov cx, 200+w
+ mov dx, 145+h
+ mov al, 30
+u2145: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2145
+ 
+  mov cx, 200+w
+ mov dx, 144+h
+ mov al, 30
+u2144: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2144
+ 
+ 
+  mov cx, 200+w
+ mov dx, 143+h
+ mov al, 30
+u2143: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2143
+ 
+  mov cx, 200+w
+ mov dx, 142+h
+ mov al, 30
+u2142: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2142
+ 
+  mov cx, 200+w
+ mov dx, 141+h
+ mov al, 30
+u2141: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2141
+ 
+ 
+ 
+  mov cx, 200+w
+ mov dx, 140+h
+ mov al, 30
+u2140: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2140
+ 
+ 
+ 
+ 
+ 
+ 
+  mov cx, 200+w
+ mov dx, 139+h
+ mov al, 30
+u2139: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2139
+ 
+  mov cx, 200+w
+ mov dx, 138+h
+ mov al, 30
+u2138: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2138
+ 
+  mov cx, 200+w
+ mov dx, 137+h
+ mov al, 30
+u2137: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2137
+ 
+  mov cx, 200+w
+ mov dx, 136+h
+ mov al, 30
+u2136: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2136
+ 
+ 
+  mov cx, 200+w
+ mov dx, 135+h
+ mov al, 30
+u2135: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2135
+ 
+  mov cx, 200+w
+ mov dx, 134+h
+ mov al, 30
+u2134: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2134
+ 
+ 
+  mov cx, 200+w
+ mov dx, 133+h
+ mov al, 30
+u2133: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2133
+ 
+  mov cx, 200+w
+ mov dx, 132+h
+ mov al, 30
+u2132: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2132
+ 
+  mov cx, 200+w
+ mov dx, 131+h
+ mov al, 30
+u2131: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2131
+ 
+ 
+ 
+  mov cx, 200+w
+ mov dx, 130+h
+ mov al, 30
+u2130: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2130
+ 
+ 
+ ;nivel urmator
+ 
+ mov cx, 200+w
+ mov dx, 129+h
+ mov al, 30
+u2129: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2129
+ 
+  mov cx, 200+w
+ mov dx, 128+h
+ mov al, 30
+u2128: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2128
+ 
+  mov cx, 200+w
+ mov dx, 127+h
+ mov al, 30
+u2127: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2127
+ 
+  mov cx, 200+w
+ mov dx, 126+h
+ mov al, 30
+u2126: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2126
+ 
+ 
+  mov cx, 200+w
+ mov dx, 125+h
+ mov al, 30
+u2125: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2125
+ 
+  mov cx, 200+w
+ mov dx, 124+h
+ mov al, 30
+u2124: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2124
+ 
+ 
+  mov cx, 200+w
+ mov dx, 123+h
+ mov al, 30
+u2123: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2123
+ 
+  mov cx, 200+w
+ mov dx, 122+h
+ mov al, 30
+u2122: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2122
+ 
+  mov cx, 200+w
+ mov dx, 121+h
+ mov al, 30
+u2121: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2121
+ 
+ 
+ 
+  mov cx, 200+w
+ mov dx, 120+h
+ mov al, 30
+u2120: mov ah, 0ch
+ int 10h
+ dec cx
+ cmp cx, 100
+ ja u2120
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+ 
+ 
  
  
  
