14c531506e47dfe1a919c4c8f0b966cba934f78f
mariansurdu <surdu_marian94@yahoo.com>
2016-04-13 22:43:07 +0300

    'Adaugare_opt_mouse'

--- smp.asm
+++ smp.asm
@@ -11,11 +11,11 @@ include 'emu8086.inc'
 
 msg1    db      10, 13, 10, 13, "Please select an item:",0Dh,0Ah,0Dh,0Ah,09h
 
-        db      "1- Graphics",0Dh,0Ah,09h
+        db      "1- Installation ",0Dh,0Ah,09h
 
         db      "2- About",0Dh,0Ah,09h     
 
-        db      "3- Something",0Dh,0Ah,09h    
+        db      "3- Draw with Mouse",0Dh,0Ah,09h    
 
         db      "4- Exit",0dh,0ah,09h
 
@@ -92,7 +92,7 @@ getnum:
 
     cmp     al, "3"
 
-    je     Quit         
+    je     Draw         
 
     
     
@@ -120,8 +120,83 @@ Showabout:
 
     int     21h   
 
-    jmp     ShowMenu  
+    jmp     ShowMenu    
     
+    
+    
+Draw:
+
+jmp start
+oldX dw -1
+oldY dw 0
+start:
+mov ah, 00
+mov al, 13h ; intrare in mod grafic rezolutia 320x200
+int 10h
+
+mov ax, 0 ; are loc initializarea mouse-ului
+int 33h
+cmp ax, 0
+mov ax, 1 ; afisare cursor mouse
+int 33h 
+
+        check_mouse_button:
+        mov ax, 3
+        int 33h 
+        shr cx, 1 
+        cmp bx, 1
+        jne xor_cursor:
+        mov al, 100b ; culoare punct
+        jmp draw_pixel  ;desenare pixel
+        
+        xor_cursor:
+        cmp oldX, -1
+        je not_required
+        push cx
+        push dx
+        mov cx, oldX
+        mov dx, oldY
+        mov ah, 0dh
+        int 10h
+        xor al, 1111b
+        mov ah, 0ch
+        int 10h
+        pop dx
+        pop cx  
+        
+        
+        not_required:
+        mov ah, 0dh
+        int 10h
+        xor al, 1111b
+        mov oldX, cx
+        mov oldY, dx
+
+        draw_pixel:
+        mov ah, 0ch
+        int 10h   
+        
+        check_esc_key:
+        mov dl, 255
+        mov ah, 6
+        int 21h
+        cmp al, 27 ; 
+        jne check_mouse_button
+
+        stop:
+        mov ax, 3 ; BACK IN NORMAL
+        int 10h
+        mov ah, 1
+        mov ch, 0
+        mov cl, 8
+        int 10h
+        mov dx, offset msg
+        mov ah, 9
+        int 21h
+        mov ah, 0
+        int 16h
+hlt
+msg db " press any key.... $"
     
  
 
