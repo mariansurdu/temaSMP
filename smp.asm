312219211501084235f52034abb620aae61c5869
mariansurdu <surdu_marian94@yahoo.com>
2016-04-13 14:19:33 +0300

    'awd'

--- smp.asm
+++ smp.asm
@@ -1,4 +1,6 @@
 
+include 'emu8086.inc'
+
 .model small
 
 .stack 100h
@@ -9,13 +11,13 @@
 
 msg1    db      10, 13, 10, 13, "Please select an item:",0Dh,0Ah,0Dh,0Ah,09h
 
-        db      "1- Draw Rectangle",0Dh,0Ah,09h
+        db      "1- Graphics",0Dh,0Ah,09h
 
         db      "2- About",0Dh,0Ah,09h     
 
-        db      "3- Exit",0Dh,0Ah,09h    
+        db      "3- Something",0Dh,0Ah,09h    
 
-        db      "4- Open File",0dh,0ah,09h
+        db      "4- Exit",0dh,0ah,09h
 
         db      "Enter item number: "
 
@@ -23,20 +25,19 @@ msg1    db      10, 13, 10, 13, "Please select an item:",0Dh,0Ah,0Dh,0Ah,09h
 
          
 
-About   db      10, 13, 10, 13, "This program is used to create a file,read a file and to serve some details about the program!!$"
+About   db      10, 13, 10, 13, "This program shows us some graphics in emu8086!! :)$"
+
+    
+
+
+
+
+
+
+
+
 
  
-
-handle  dw  ?
-
-file1   db  "c:\smp.txt", 0
-
-text    db  "This Message Contains Absolutely Nothing!",0
-
-text_size equ $ - text
-
-   
-
 .code
 
 main proc
@@ -71,28 +72,38 @@ getnum:
 
     cmp     al, '3'
 
-    jg      ShowMenu
-
-         
+    jg      ShowMenu 
+    
+    
 
     cmp     al, "1"
 
     je      DrawRectangle
 
-    cmp     al, "2"
-
+      
+      
+      
+    cmp     al, "2"   
     je      ShowAbout
+    
+                   
+                   
+                   
 
     cmp     al, "3"
 
-    jmp     Quit         
+    je     Quit         
 
+    
+    
+    
     cmp     al, "4"
 
-    jmp     OpenFile
+    je     Quit
 
          
-
+ 
+ 
 Quit:
 
    mov   ah,4ch
@@ -109,13 +120,13 @@ Showabout:
 
     int     21h   
 
-    jmp     ShowMenu
+    jmp     ShowMenu  
+    
+    
+ 
 
-     
 
 DrawRectangle:
-
-jmp new
 
 w equ 20 ; dimensiune dreptunghi
 h equ 6
@@ -156,12 +167,7 @@ u3: mov ah, 0ch
  cmp dx, 100
  ja u4
  
- 
- 
- 
- 
- 
-       ; afisare latura superioara
+; afisare latura superioara
  mov cx, 200+w ; coloana
  mov dx, 100 ; rand
  mov al, 40 ; culoare
@@ -177,72 +183,15 @@ u1: mov ah, 0ch ; afisare pixel
  ; asteptare apasare tasta
  mov ah,00
  int 16h  
- jmp     ShowMenu
+ jmp   ShowMenu
+
+
 
 
 ret
-
-               
-
-OpenFile:   
-
-jmp print
-
-mov dx, offset file
-
-mov al,0
-
-mov ah,3dh
-
-int 21h
-
-jc terminate
-
-mov bx,ax
-
-mov cx,1
-
-print:
-
-lea dx, BUF
-
-mov ah,3fh
-
-int 21h
-
-CMP AX, 0
-
-JZ terminate
-
-mov al, BUF
-
-mov ah,0eh
-
-int 10h
-
-jmp print
-
-terminate:
-
-mov ah, 0
-
-int 16h
-
-ret
-
-file db "c:\smp.txt", 0
-
-BUF db ?
-
-END           
-
- 
-
-   jmp     ShowMenu               
 
 main endp
 
 end main
 
 
-These were the new codes I put in
